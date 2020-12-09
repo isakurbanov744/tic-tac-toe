0326a4aa6ee53e5b4dc5d9057fd986d050e0bf24
isakurbanov744 <isakurbanov75@gmail.com>
2020-12-09 22:23:48 +0400

    Deleted React Files

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+            //set state
+        }
+    }
+    render() { 
+        return (
+            <>
+            <div>
+                Hello Man
+            </div>
+            </>
+        );
+    }
+}
+ 
+export default App;
